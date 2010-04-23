8042aa67c65563a466eef96a70baf76019d27b18
graham <devnull@localhost>
2010-04-23 14:16:12 +0000

    *** empty log message ***

--- src/main/org/h2/h2o/remote/ChordRemote.java
+++ src/main/org/h2/h2o/remote/ChordRemote.java
@@ -740,7 +740,7 @@ public class ChordRemote implements IDatabaseRemote, IChordInterface, Observer {
 
 
 		if (!Constants.IS_NON_SM_TEST){
-			((ChordNodeImpl)chordNode).destroy();
+			((ChordNodeImpl)chordNode).shutDown();
 		}
 	}
 
