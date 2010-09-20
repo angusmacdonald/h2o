4bd9f9d9ad5cc2b0050c483f845cebba1b2d3ff9
Angus Macdonald <amacdonald@aetherworks.com>
2010-09-20 12:46:28 +0000

    Updated the delete script to include proper project name.

--- service/delete.bat
+++ service/delete.bat
@@ -1,2 +1,2 @@
-rmdir /s /q C:\Users\angus\workspace\h2o-working\db_data
-rmdir /s /q C:\Users\angus\workspace\h2o-working\data
+rmdir /s /q C:\Users\angus\workspace\h2o\db_data
+rmdir /s /q C:\Users\angus\workspace\h2o\data
