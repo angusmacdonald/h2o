2c9b254f16296ff554963d6ad51cde2bdea41249
graham <devnull@localhost>
2010-09-22 10:33:49 +0000

    *** empty log message ***

--- src/test/org/h2o/run/CheckInTests.java
+++ src/test/org/h2o/run/CheckInTests.java
@@ -48,8 +48,8 @@ import org.junit.runners.Suite;
 	IndexTests.class,
 //	UpdateTests.class
 //	MultiQueryTransactionTests.class
-	H2Tests.class
-//	WrapperTests.class,
+//	H2Tests.class
+	WrapperTests.class
 //	RestartTests.class,
 //	LocatorTests.class
 })
