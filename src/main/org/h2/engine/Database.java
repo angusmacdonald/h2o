8b6b5b4b2bc65976d6daaadb65afa220e68ba523
Angus Macdonald <amacdonald@aetherworks.com>
2011-05-27 10:59:56 +0100

    Turned off 'ignore numonic diagnostics' option to try and see why it doesn't seem to be sending monitoring data on linux.

--- src/main/org/h2/engine/Database.java
+++ src/main/org/h2/engine/Database.java
@@ -3317,7 +3317,7 @@ public class Database implements DataHandler, Observer {
 
         Diagnostic.addIgnoredPackage("uk.ac.standrews.cs.stachord");
         Diagnostic.addIgnoredPackage("uk.ac.standrews.cs.nds");
-        Diagnostic.addIgnoredPackage("uk.ac.standrews.cs.numonic");
+        //Diagnostic.addIgnoredPackage("uk.ac.standrews.cs.numonic");
         Diagnostic.setTimestampFlag(true);
         Diagnostic.setTimestampFormat(new SimpleDateFormat("HH:mm:ss:SSS "));
         Diagnostic.setTimestampDelimiterFlag(false);
