2fcb79b2e3c2be2f1f4c0e603ae1deff6dee50c7
Angus Macdonald <amacdonald@aetherworks.com>
2011-06-06 15:28:03 +0100

    Added stub interfaces for the evaluation framework.

---
+++ src/test/org/h2o/eval/interfaces/IWorker.java
@@ -0,0 +1,65 @@
+package org.h2o.eval.interfaces;
+
+/**
+ * Worker classes register with a central co-ordinator (implementing {@link ICoordinator}) 
+ * and accept requests to start/stop a local H2O instance, and to start/stop workloads which query these instances.
+ * 
+ *
+ * @author Angus Macdonald (angus.macdonald@st-andrews.ac.uk)
+ */
+public interface IWorker {
+
+    /*
+     * H2O Instance-Related Methods.
+     */
+
+    /**
+     * Start a new H2O instance which will connect to the database system described by the database descriptor file provided.
+     * @param descriptorFileLocation Location of the database descriptor file (can be a URI or a local file system path).
+     * @return true if an H2O instance was successfully started.
+     */
+    public boolean startH2OInstance(final String descriptorFileLocation);
+
+    /**
+     * Gracefully shutdown the H2O instance that this worker is running (by sending it a shutdown request).
+     * @return true if the H2O was successfully stopped. In cases where it couldn't be stopped or wasn't even running, false is returned.
+     */
+    public boolean stopH2OInstance();
+
+    /**
+     * Abruptly terminate the H2O instance that is running locally (by issuing a kill command on the process).
+     * @return true if the H2O was successfully stopped. In cases where it couldn't be stopped or wasn't even running, false is returned.
+     */
+    public boolean terminateH2OInstance();
+
+    /**
+     * Whether the worker is running an H2O instance locally.
+     * @return true if the worker has started an instance, false if it hasn't.
+     */
+    public boolean isH2OInstanceRunning();
+
+    /*
+     * Workload-related methods.
+     */
+
+    /**
+     * Start the given workload on this worker by executing it against the local H2O instance.
+     * @param workload Workload to be executed against the local H2O instance.
+     * @return true if the workload was started successfully.
+     */
+    public boolean startWorkload(IWorkload workload);
+
+    /**
+     * Stop the given workload from executing on this worker node.
+     * @param workload The workload that should be stopped.
+     * @return True if the workload was stopped, false it it couldn't be stopped or if it was not running.
+     */
+    public boolean stopWorkload(IWorkload workload);
+
+    /**
+     * Whether the specified workload is running on this worker.
+     * @param workload The workload that this method tests for.
+     * @return True if the workload is running, false it it isn't.
+     */
+    public boolean isWorkloadRunning(IWorkload workload);
+}
---
+++ src/test/org/h2o/eval/interfaces/IWorkload.java
@@ -0,0 +1,10 @@
+package org.h2o.eval.interfaces;
+
+/**
+ * Workload to be run on a worker node.
+ *
+ * @author Angus Macdonald (angus.macdonald@st-andrews.ac.uk)
+ */
+public interface IWorkload {
+
+}
